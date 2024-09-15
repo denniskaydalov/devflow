3b575f190f54fd752f162b9fb07403ccb9c440ec
KarmenM18 <karmenjot.minhas@gmail.com>
2024-09-14 20:03:54 -0400

    add ticketlist component

---
+++ devflow/src/components/TicketList.tsx
@@ -0,0 +1,31 @@
+import { List, SearchInput } from "react-admin";
+import { useMediaQuery, Typography, Theme } from "@mui/material";
+import React from "react"; // added this import to keep it functional
+
+import { TicketListContent } from ".";
+
+const ticketFilters = [
+  // eslint-disable-next-line react/jsx-key
+  <SearchInput source="q" alwaysOn />,
+];
+
+export const TicketList = () => {
+  const isSmall = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
+  return (
+    <List
+      filters={ticketFilters}
+      perPage={100}
+      sort={{ field: "index", order: "ASC" }}
+      pagination={false}
+      component="div"
+    >
+      {isSmall ? <FallbackForMobile /> : <TicketListContent />}
+    </List>
+  );
+};
+
+const FallbackForMobile = () => (
+  <Typography mt={3} align="center">
+    The Kanban board demo is not available on mobile
+  </Typography>
+);
